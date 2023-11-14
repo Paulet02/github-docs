c3d4e79b176e26226de0fd253fff37a1688a7638
Paulet <josepaulet@hotmail.es>
2023-11-14 05:23:36 +0100

    Initial commit

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ release = '0.0.1'
 
 import mock
 
-MOCK_MODULES = ['numpy', 'sympy', 'json', 'logging', 're', 'requests', 'requests.exceptions', 'requests.packages']
+MOCK_MODULES = ['numpy', 'sympy', 'json', 'logging', 're', 'requests', 'requests.exceptions', 'requests.packages', 'requests.packages.urllib3']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
